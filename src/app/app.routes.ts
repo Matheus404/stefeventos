7378d44f58efe321db19b84bea1a2700a6c1ac9f
Jose Matheus Avelino De Souza Vidal <jmvidal@stefanini.com>
2024-10-22 11:04:51 -0300

    iniciando projeto do site de eventos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { SobreComponent } from './pagina/sobre/sobre.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: InicioComponent},
+    {path: 'inicio', component: InicioComponent},
+    {path: 'sobre', component: SobreComponent},
+    {path: 'contato', component: ContatoComponent}
+
+];
